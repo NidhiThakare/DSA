bf287d29bcf50c81f2ae8ba87498e0e501ee0e25
Nidhi Thakare <8d.nidhithakare@gmail.com>
2026-07-06 21:06:48 +0530

    ASCII Value of a Character

---
+++ ascii_value.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout << "Enter a character to find its ASCII value: ";
+    cin >> ch;
+
+    cout << "The ASCII value of " << ch << " is: " << int(ch) << endl;
+    return 0;
+}
