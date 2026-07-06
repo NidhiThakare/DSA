c8d653e913ad3aabd9d1b047af10da93a74fd14e
Nidhi Thakare <8d.nidhithakare@gmail.com>
2026-07-07 00:19:42 +0530

    Armstrong No.

---
+++ armstrong_no.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, temp, rem, sum = 0;
+
+    cout << "Enter a Num: ";
+    cin >> n; 
+
+    temp = n;
+
+    while(temp != 0){
+        rem = temp % 10;
+        sum = sum + (rem*rem*rem);
+        temp = temp / 10;
+    }
+    
+    if(sum == n){
+        cout << n << " is an Armstrong number." << endl;
+    }
+    else{
+        cout << n << " is not an Armstrong number." << endl;
+    }
+    return 0;
+}
